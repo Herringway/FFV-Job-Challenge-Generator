c6bb5cefbf22cd72676c452e06c46fac57c89fdd
Cameron Ross <elpenguino@gmail.com>
2012-05-27 17:40:30 -0300

    result now based on ip address

--- index.php
+++ index.php
@@ -7,9 +7,12 @@ $jobs[] = array('Time Mage', 'Magic Knight', 'Summoner', 'Berserker', 'Red Mage'
 $jobs[] = array('Ninja', 'Elementalist', 'Bard', 'Ranger', 'Trainer');
 $jobs[] = array('Samurai', 'Dragoon', 'Chemist', 'Dancer');
 
-if (array_key_exists('advancemode', $_GET))
+if (isset($_GET['advancemode']))
 	$jobs[] = array('Oracle', 'Cannoneer', 'Gladiator');
 
+if (!isset($_GET['trand']))
+	srand(ip2long($_SERVER['REMOTE_ADDR']));
+	
 $selectedjobs = array();
 foreach ($jobs as $j)
 	$selectedjobs[] = array('job' => $j[rand(0,count($j)-1)]);
