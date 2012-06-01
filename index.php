3e650ac13bc74e018b8459acd1c2f8474ffa9cda
Cameron Ross <elpenguino@gmail.com>
2012-06-01 16:29:33 -0300

    add hard mode support

--- index.php
+++ index.php
@@ -7,15 +7,23 @@ $jobs[] = array('Time Mage', 'Magic Knight', 'Summoner', 'Berserker', 'Red Mage'
 $jobs[] = array('Ninja', 'Elementalist', 'Bard', 'Ranger', 'Trainer');
 $jobs[] = array('Samurai', 'Dragoon', 'Chemist', 'Dancer');
 
-if (isset($_GET['advancemode']))
-	$jobs[] = array('Oracle', 'Cannoneer', 'Gladiator');
-
 if (!isset($_GET['trand']))
 	srand(ip2long($_SERVER['REMOTE_ADDR']));
 	
-$selectedjobs = array();
-foreach ($jobs as $j)
-	$selectedjobs[] = array('job' => $j[rand(0,count($j)-1)]);
+if (!isset($_GET['hard'])) {
+	if (isset($_GET['advancemode']))
+		$jobs[] = array('Oracle', 'Cannoneer', 'Gladiator');
 
+		
+	$selectedjobs = array();
+	foreach ($jobs as $j)
+		$selectedjobs[] = array('job' => $j[rand(0,count($j)-1)]);
+} else {
+	for($i = 1; $i < 4; $i++)
+		$jobs[$i] = array_merge($jobs[$i], $jobs[$i-1]);
+
+	foreach ($jobs as $j)
+		$selectedjobs[] = array('job' => $j[rand(0,count($j)-1)]);
+}
 $dwoo->output('ffvjobs.tpl', array('jobs' => $selectedjobs));
 ?>
