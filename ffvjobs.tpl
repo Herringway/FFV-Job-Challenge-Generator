21868154e98f16683d98c229e933771183749c73
Cameron Ross <elpenguino@gmail.com>
2014-03-06 15:43:21 -0400

    Switch from dwoo to twig, implement BERSERKERRISK, magical, physical, mime, single job modes and allow them to be used with each other

--- ffvjobs.tpl
+++ ffvjobs.tpl
@@ -2,9 +2,25 @@
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 	<head>
 		<title>FFV Random Jobs</title>
-		<link rel="shortcut icon" href="ffvsprites/knight.PNG" />
+		<link rel="shortcut icon" href="sprites/butz/knight.png" />
+		<style>
+		body {background: lightgray;}
+		th   {text-align: center; width: 100px; background: white;}
+		td   {text-align: center; width: 100px; background: white;}
+		</style>
 	</head>
-	<body style="background: lightgray;">
-		<table><tr>{loop $jobs}<td style="text-align: center; width: 100px; background: white;"><img alt="{$job}" src="ffvsprites/{$character}/{replace($job, ' ', '%20')|lower}.PNG"><br>{$job}</td>{/loop}</tr></table>
+	<body>
+		<table>
+			<tr>{%for crystal,job in jobs%}
+
+				<th>{{crystal|capitalize}}</th>{%endfor%}
+
+			</tr>
+			<tr>{%for crystal,job in jobs%}
+
+				<td><img alt="{{job.job|capitalize}}" src="sprites/{{job.character}}/{{job.job|replace({' ':'%20'})}}.png"><br>{{job.job|capitalize}}</td>{%endfor%}
+
+			</tr>
+		</table>
 	</body>
 </html>
--- index.php
+++ index.php
@@ -1,31 +1,65 @@
 <?php
-require_once 'Dwoo/dwooAutoload.php';
-$dwoo = new Dwoo();
-$jobs = array();
-$jobs[] = array('Knight', 'Monk', 'Thief', 'White Mage', 'Black Mage', 'Blue Mage');
-$jobs[] = array('Time Mage', 'Mystic Knight', 'Summoner', 'Berserker', 'Red Mage', 'Mime');
-$jobs[] = array('Ninja', 'Geomancer', 'Bard', 'Ranger', 'Trainer');
-$jobs[] = array('Samurai', 'Dragoon', 'Chemist', 'Dancer');
-$characters = array('butz', 'lenna', 'faris', 'krile');
+require_once 'Twig/Autoloader.php';
 if (!isset($_GET['trand']))
-	srand(ip2long($_SERVER['REMOTE_ADDR']) + isset($_GET['superhard']));
+	srand(ip2long($_SERVER['REMOTE_ADDR']) + isset($_GET['random']) + isset($_GET['advanced']) + isset($_GET['natural']));
+$jobs = [];
+$jobs['wind'] = ['knight', 'monk', 'thief', 'white mage', 'black mage', 'blue mage'];
+$jobs['water'] = ['time mage', 'mystic knight', 'summoner', 'berserker', 'red mage'];
+$jobs['fire'] = ['ninja', 'geomancer', 'bard', 'ranger', 'trainer'];
+$jobs['earth'] = ['samurai', 'dragoon', 'chemist', 'dancer'];
+if (isset($_GET['natural']))
+	$characters = ['wind' => 'butz', 'water' => 'lenna', 'fire' => 'faris', 'earth' => 'krile', 'advanced' => 'butz'];
+else
+	$characters = ['wind' => 'butz', 'water' => 'butz', 'fire' => 'butz', 'earth' => 'butz', 'advanced' => 'butz'];
+if (isset($_GET['alsomime']))
+	$jobs['water'][] = 'mime';
 	
-if (isset($_GET['hard'])) {
-	for($i = 1; $i < 4; $i++)
-		$jobs[$i] = array_merge($jobs[$i], $jobs[$i-1]);
+if (isset($_GET['advanced']))
+	$jobs['advanced'] = ['oracle', 'cannoneer', 'gladiator'];
 
-	foreach ($jobs as $j)
-		$selectedjobs[] = array('character' => 'butz', 'job' => $j[rand(0,count($j)-1)]);
-} else if (isset($_GET['superhard'])) {
-	foreach ($jobs as $i => $j)
-		$selectedjobs[] = array('character' => $characters[$i], 'job' => $j[rand(0,count($j)-1)]);
-} else {
-	if (isset($_GET['advancemode']))
-		$jobs[] = array('Oracle', 'Cannoneer', 'Gladiator');
 
-	$selectedjobs = array();
-	foreach ($jobs as $j)
-		$selectedjobs[] = array('character' => 'butz', 'job' => $j[rand(0,count($j)-1)]);
+if (isset($_GET['magical'])) {
+	$jobs['wind'] = array_intersect($jobs['wind'], ['white mage', 'black mage', 'blue mage']);
+	$jobs['water'] = array_intersect($jobs['water'], ['time mage', 'summoner', 'red mage']);
+	$jobs['fire'] = array_intersect($jobs['fire'], ['bard', 'geomancer']);
+	$jobs['earth'] = array_intersect($jobs['earth'], ['chemist', 'dancer']);
+	if (isset($jobs['advanced']))
+		$jobs['advanced'] = array_intersect($jobs['advanced'], ['oracle']);
 }
-$dwoo->output('ffvjobs.tpl', array('jobs' => $selectedjobs));
+if (isset($_GET['physical'])) {
+	$jobs['wind'] = array_intersect($jobs['wind'], ['knight', 'monk', 'thief']);
+	$jobs['water'] = array_intersect($jobs['water'], ['mystic knight', 'berserker']);
+	$jobs['fire'] = array_intersect($jobs['fire'], ['ranger', 'trainer', 'ninja']);
+	$jobs['earth'] = array_intersect($jobs['earth'], ['samurai', 'dragoon']);
+	if (isset($jobs['advanced']))
+		$jobs['advanced'] = array_intersect($jobs['advanced'], ['gladiator', 'cannoneer']);
+}
+	
+if (isset($_GET['onejob'])) {
+	$selected = $jobs['wind'][array_rand($jobs['wind'])];
+	foreach ($jobs as &$joblist)
+		$joblist = [$selected];
+}
+	
+$selectedjobs = [];
+
+if (isset($_GET['random'])) {
+	$availjobs = [];
+	foreach($jobs as $crystal=>$joblist) {
+		$availjobs = array_merge($availjobs, $joblist);
+		$jobs[$crystal] = $availjobs;
+	}
+}
+if (isset($_GET['BERSERKER']))
+	$jobs['water'] = ['berserker'];
+foreach ($jobs as $crystal => $j)
+	if (count($j) >= 1)
+		$selectedjobs[$crystal] = ['character' => $characters[$crystal], 'job' => $j[array_rand($j)]];
+
+Twig_Autoloader::register();
+
+$loader = new Twig_Loader_Filesystem('.');
+$twig = new Twig_Environment($loader);
+$template = $twig->loadTemplate('ffvjobs.tpl');
+$template->display(['jobs' => $selectedjobs]);
 ?>
